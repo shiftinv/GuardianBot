3c4ec682c1d7072d01a5c13f35cdb29a0652b0e4
shiftinv <me@shiftinv.cc>
2021-12-31 20:22:15 +0100

    Automatically clean spam history

--- guardianbot/filter/spam_checker.py
+++ guardianbot/filter/spam_checker.py
@@ -3,9 +3,9 @@ import re
 import disnake
 import logging
 from pydantic import Field
-from datetime import timedelta
+from datetime import datetime, timedelta
 from collections import defaultdict
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, Iterable, List, Optional, Tuple
 
 from .. import utils
 from ._base import ManualBaseChecker, CheckResult
@@ -26,8 +26,31 @@ class SpamChecker(ManualBaseChecker):
 
         # (author ID, message content) -> list of messages
         self.history: Dict[Tuple[int, str], List[disnake.PartialMessage]] = defaultdict(list)
+        self.__last_clear = utils.utcnow()
 
     async def check_match(self, msg: disnake.Message) -> Optional[CheckResult]:
+        now = msg.created_at
+        min_spam_time = now - timedelta(seconds=self.config.interval_sec)
+
+        if self.__last_clear < now - timedelta(seconds=5):
+            dropped = 0
+
+            for k, hist in list(self.history.items()):
+                new_hist = list(self.__clean_history(hist, min_spam_time))
+                if len(new_hist) == 0:
+                    # if new history empty, drop the entire thing
+                    self.history.pop(k)
+                    dropped += len(hist)
+                elif len(new_hist) != len(hist):
+                    # if new history changed, overwrite old one
+                    self.history[k] = new_hist
+                    assert len(new_hist) < len(hist)
+                    dropped += len(hist) - len(new_hist)
+
+            if dropped:
+                logger.debug(f'cleaned {dropped} history entries')
+            self.__last_clear = now
+
         for r in self:
             if match := re.search(r, msg.content, re.MULTILINE):
                 created = msg.created_at
@@ -43,11 +66,8 @@ class SpamChecker(ManualBaseChecker):
 
                 # drop older history entries
                 before = len(hist)
-                hist[:] = itertools.dropwhile(
-                    lambda m: m.created_at < created - timedelta(seconds=self.config.interval_sec),
-                    hist
-                )
-                logger.debug(f'dropped {before - len(hist)} history entries')
+                hist[:] = self.__clean_history(hist, min_spam_time)
+                logger.debug(f'dropped {before - len(hist)} matching history entries')
 
                 if len(hist) >= self.config.repeat_count:
                     diff = (created - hist[0].created_at).seconds
@@ -56,3 +76,7 @@ class SpamChecker(ManualBaseChecker):
 
                 break  # don't continue searching since spam detection is based on message content, not the specific regex that matched
         return None
+
+    @staticmethod
+    def __clean_history(history: List[disnake.PartialMessage], min_time: datetime) -> Iterable[disnake.PartialMessage]:
+        return itertools.dropwhile(lambda m: m.created_at < min_time, history)
